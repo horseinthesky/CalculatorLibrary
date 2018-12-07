49ab8e0351f1a91f77b5962a2d324e7a0b7f5811
horseinthesky <pwnedbyspawn@gmail.com>
2018-12-07 12:48:46 +0300

    Add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Calculator library containing basic math operations.
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
